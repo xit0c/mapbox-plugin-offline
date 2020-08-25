d8bf11a3f67b1dd2c47c0971f366e6881de52023
Michele Ferretti <michele.ferretti.93@gmail.com>
2020-08-25 02:01:11 +0200

    Kotlin, Gradle, Mapbox, AndroidX AppCompat, AndroidX Core and AndroidX ConstraintLayout updated; Mapbox dependency is now included with the new authentication mechanism

--- app/src/main/java/dev/micheleferretti/mapboxpluginofflinedemo/DemoApp.kt
+++ app/src/main/java/dev/micheleferretti/mapboxpluginofflinedemo/DemoApp.kt
@@ -2,7 +2,6 @@ package dev.micheleferretti.mapboxpluginofflinedemo
 
 import android.app.Application
 import com.mapbox.mapboxsdk.Mapbox
-import dev.micheleferretti.mapboxpluginoffline.BuildConfig
 
 class DemoApp: Application() {
 
